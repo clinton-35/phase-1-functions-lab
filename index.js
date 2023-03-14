91ea9cd355efb5bf247b02b0009e9b3c4d13a1a4
clinton <clinton.wambua@student.moringaschool.com>
2023-03-14 14:21:05 +0300

    assignment complete

--- index.js
+++ index.js
@@ -1,1 +1,44 @@
 // Code your solution in this file!
+function distanceFromHqInBlocks (street) {
+    //returns the number of blocks given a value
+    if (street<42){
+      return(42-street);
+    }
+    else {
+  
+    return(street-42);
+  }
+  }
+  
+  
+  
+  function distanceFromHqInFeet (street) {
+    return distanceFromHqInBlocks(street)*264;
+    // calls distanceFromHqInBlocks from inside the distanceFromHqInFeet function, passing the argument from distanceFromHqInFeet into distanceFromHqInBlocks
+    //the return value of distanceFromHqInBlocks can then be used to calculate feet
+  }
+  
+  function distanceTravelledInFeet(a,b){
+    if (b>a){
+      return((b-a)*264);
+    }
+    else
+      return((a-b)*264);
+  }
+  
+  
+  function calculatesFarePrice(start,destination){
+    let distance = distanceTravelledInFeet(start,destination);
+    if (distance<= 400){
+      return 0;
+    }
+    if (distance>=400 && distance<=2000){
+      return (distance-400)* 0.02;
+    }
+    else if(distance>=2000&& distance<=2500){
+      return 25;
+    }
+    else if (distance > 2500){
+      return "cannot travel that far";
+    }
+  }
